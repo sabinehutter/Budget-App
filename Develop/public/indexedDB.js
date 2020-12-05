5a85e00c3837b677d4b6d364653fffa4e0a70d25
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-05 14:26:48 -0500

    sudo coding

--- Develop/public/indexedDB.js
+++ Develop/public/indexedDB.js
@@ -1,6 +1,15 @@
 function saveRecord(data){
     console.log(data)
 
-    // Above ^ register service worker (outside the script)
-    // 
+    // Above ^ register service worker
+    // check for service worrk (if self is registered then sw is availabelt o us)
+
+
+    // SW lifecycle
+        // Install -> create bulk (pending) collection 
+        // activate -> 
+
+        // saveRecord -> save to indexedBD
+
+        // listen to when back online and send records 
 }
