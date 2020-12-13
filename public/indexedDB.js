74fcc965e25cb6d9a24849da0569ba30e2ed4d0e
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-12 22:46:20 -0500

    storing records

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,26 +1,47 @@
-function saveRecord(data){
-    console.log(data)
+const indexedDB = 
+window.indexedDB ||
+window.mozIndexedDB ||
+window.webkitIndexedDB ||
+window.msIndexedDB ||
+window.shimIndexedDB;
 
     // create budet db
 
+let db;
 
-        // Install -> create bulk (pending) collection 
-        // activate -> 
+// request
+const request = indexedDB.open('budget', 1);
 
-        // saveRecord -> save to indexedBD
+// request on upgrade 
+// Install -> create bulk (pending) collection 
 
-        // listen to when back online and send records 
+request.onupgradeneeded = ({target}) => {
+    db = target.result;
+    db.createObjectStore("pending", {autoIncrement : true})
+}
 
-        let db;
-        const reqeest = indexedDB.open("budget", 1)
+// request on success
+// listen to when back online and send records 
+request.onsuccess = ({target}) => {
+    db = target.result;
+    if (navigator.onLine){
+        checkDatabase();
+    }
+}
 
-        requestAnimationFrame.onerror()
+request.onerror = function(event){
+    console.log("Request Error")
+}
 
-        function saveRecord(){}
+ // saveRecord -> save to indexedBD
+function saveRecord(entry){
+    const transation = db.transation(['pending'], "readwrite");
+
+    const storeData = transation.objectStore('pending')
+
+    storeData.add(entry)
+}
 
         function checkDatabase(){
 
         }
-
-
-}
